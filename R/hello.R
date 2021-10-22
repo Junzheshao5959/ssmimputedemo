cd61dcb59cb975d31bd0fc34edd2c063bfea69ca
Junzheshao5959 <js5959@cumc.columbia.edu>
2021-10-22 00:15:18 -0400

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' My hello world function
+#' @param x The name of person to say hi to
+#'
+#' @return The output from  \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("Steve")
+#' \dontrun{
+#' hello("Steve")}
+hello <- function(x) {
+  print("Hello",x,", this is the word world!")
+}
