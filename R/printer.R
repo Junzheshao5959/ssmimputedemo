8ca6cc7692c63c312bc864015858bb400d360b46
Junzheshao5959 <js5959@cumc.columbia.edu>
2021-10-22 12:43:09 -0400

    update for printer

--- R/printer.R
+++ R/printer.R
@@ -1,13 +1,14 @@
 #' test printer
 #'
-#' @param r
-#' @param x
+#' @param r Variable r
+#' @param x Variable x
 #'
 #' @return A tibble
 #' @export
 #' @importFrom tibble as_data_frame
+#' @import dplyr
 #' @examples
-#' printer(x = rnonrm(5), r = rnorm(5))
+#' printer(x = rnorm(5), r = rnorm(5))
 printer = function(r,x){
   x = tibble::as_data_frame(x = x, y = r)
   print(head(x))
